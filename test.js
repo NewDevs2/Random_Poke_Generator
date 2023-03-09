f8bdd2d4b93f3d96b7ef367ca6fdb324ba0cbdaf
KwakYounHo <kwakyh951@gmail.com>
2023-03-09 09:58:00 +0900

    모듈 테스트

---
+++ test.js
@@ -0,0 +1,7 @@
+const react = {
+  a : 1,
+  b : 2,
+  c : function (a,b) {let result = a+b; 
+    return result;},
+}
+module.exports=react;
---
+++ test_1.js
@@ -0,0 +1,3 @@
+const react = require('./test.js')
+
+console.log(react.c(5,6));
